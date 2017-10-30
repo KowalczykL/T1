c5a87f3d4bf61227b860b7bd6c3697a35801b05c
Łukasz Kowalczyk <ukasz Kowalczyk@Ni-23-0338>
2017-10-30 13:17:16 +0100

    test gita

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -5,6 +5,13 @@
  */
 package javaapplication1;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Łukasz Kowalczyk
@@ -15,7 +22,61 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    JavaApplication1 szac2 = new JavaApplication1();
+        long wart = szac2.JavaApplication1(100, 200, "2017-01-15", "2017-02-15", "2017-03-01");
+        System.out.print(wart);
+    
     }
+    
+        public long JavaApplication1(int wskP, int wskB, String dataP, String dataB, String DataS)
+    {   
+       
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        
+        Date dataP2 = null;
+        try {
+            dataP2 = dateFormat.parse(dataP);
+        } catch (ParseException ex) {
+            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Calendar dataP2C = Calendar.getInstance();
+        dataP2C.setTime(dataP2); 
+        Date dataB2 = null;        
+        try {
+            dataB2 = dateFormat.parse(dataB);
+        } catch (ParseException ex) {
+            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Calendar dataB2C = Calendar.getInstance();
+        dataB2C.setTime(dataB2);
+        System.out.println("dataP"+dataP);
+        System.out.println("dataP2"+dataP2);
+        System.out.println("dataP2C"+dataP2C);
+        System.out.println("dataB"+dataB);
+        System.out.println("dataB2"+dataB2);
+        System.out.println("dataB2C"+dataB2C);
+    long czasWMili = dataB2C.getTimeInMillis() - dataP2C.getTimeInMillis();
+    System.out.println("czasWMiliB2C"+dataB2C.getTimeInMillis());
+    System.out.println("czasWMiliP2C"+dataP2C.getTimeInMillis());
+    System.out.println("czasWMili"+czasWMili);
+    long liczbaMSwDobie = 1000 * 60 * 60 * 24; 
+    Calendar miliPomiedzy = Calendar.getInstance();
+    System.out.println("miliPomiedzy"+miliPomiedzy);
+    miliPomiedzy.setTimeInMillis(czasWMili);  
+    System.out.println("czasWMili"+czasWMili);
+       long różnica = miliPomiedzy.getTimeInMillis()/liczbaMSwDobie;
+        System.out.println("różnica"+różnica);
+        long dzienny;
+        dzienny = (wskB - wskP)/różnica;
+        System.out.println("dzienny"+dzienny);
+        
+        
+        
+        
+        
+        
+        return  różnica;
+    }
+    
     
 }
